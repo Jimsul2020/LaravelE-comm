8faada9ed08aa5cf3610751138e936687b56b77e
Muhammed Adam <j.adinoyi.jnr@gmail.com>
2025-01-19 11:12:36 +0100

    cards content display on admin dashboard

--- app/Http/Controllers/admin/HomeController.php
+++ app/Http/Controllers/admin/HomeController.php
@@ -4,15 +4,61 @@ namespace App\Http\Controllers\admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     function index()
     {
-        return view('admin.dashboard');
-        // $admin = Auth::guard('admin')->user();
-        // echo 'Welcome' . $admin->name . ' <a href="' . route('admin.logout') . '">Logout</a>';
+        $totalOrders = Order::where('status', '!=', 'cancelled')->count();
+        $totalProducts = Product::count();
+        $totalCustomers = User::where('role', 1)->count();
+        //total revenue
+        $totalRevenue = Order::where('status', '!=', 'cancelled')->sum('grand_total');
+        
+        //This month revenue
+        $startOfMonth = Carbon::now()->startOfMonth()->format('Y-m-d');
+        $currentDate = Carbon::now()->format('Y-m-d');
+        
+        $thisMonthRevenue = Order::where('status', '!=', 'cancelled')
+        ->whereDate('created_at', '>=', $startOfMonth)
+        ->whereDate('created_at', '<=', $currentDate)
+        ->sum('grand_total');
+
+        //last month revenue
+        $lastMonthStartDate = Carbon::now()->subMonth()->startOfMonth()->format('Y-m-d');
+        $lastMonthEndDate = Carbon::now()->subMonth()->endOfMonth()->format('Y-m-d');
+        $lastMonthName = Carbon::now()->subMonth()->startOfMonth()->format('M');
+
+
+        $lastMonthRevenue = Order::where('status', '!=', 'cancelled')
+        ->whereDate('created_at', '>=', $lastMonthStartDate)
+        ->whereDate('created_at', '<=', $lastMonthEndDate)
+        ->sum('grand_total');
+
+        //last 30 days sale
+
+        $lastThirtyDaysStartDate = carbon::now()->subDays(30)->format('Y-m-d');
+
+        $lastThirtyDaysRevenue = Order::where('status', '!=', 'cancelled')
+        ->whereDate('created_at', '>=', $lastThirtyDaysStartDate)
+        ->whereDate('created_at', '<=', $currentDate)
+        ->sum('grand_total');
+
+        return view('admin.dashboard',[
+            'totalOrders' => $totalOrders,
+            'totalProducts' => $totalProducts,
+            'totalCustomers' => $totalCustomers,
+            'totalRevenue' => $totalRevenue,
+            'thisMonthRevenue' => $thisMonthRevenue,
+            'lastMonthRevenue' => $lastMonthRevenue,
+            'lastThirtyDaysRevenue' => $lastThirtyDaysRevenue,
+            'lastMonthName' => $lastMonthName
+        ]);
     }
 
     function logout()
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -22,34 +22,82 @@
 							<div class="col-lg-4 col-6">							
 								<div class="small-box card">
 									<div class="inner">
-										<h3>150</h3>
+										<h3>{{$totalOrders}}</h3>
 										<p>Total Orders</p>
 									</div>
 									<div class="icon">
 										<i class="ion ion-bag"></i>
 									</div>
-									<a href="#" class="small-box-footer text-dark">More info <i class="fas fa-arrow-circle-right"></i></a>
+									<a href="{{route('view.products')}}" class="small-box-footer text-dark">More info <i class="fas fa-arrow-circle-right"></i></a>
+								</div>
+							</div>
+							<div class="col-lg-4 col-6">							
+								<div class="small-box card">
+									<div class="inner">
+										<h3>{{$totalProducts}}</h3>
+										<p>Total Products</p>
+									</div>
+									<div class="icon">
+										<i class="ion ion-stats-bars"></i>
+									</div>
+									<a href="{{route('orders.index')}}" class="small-box-footer text-dark">More info <i class="fas fa-arrow-circle-right"></i></a>
 								</div>
 							</div>
 							
 							<div class="col-lg-4 col-6">							
 								<div class="small-box card">
 									<div class="inner">
-										<h3>50</h3>
+										<h3>{{$totalCustomers}}</h3>
 										<p>Total Customers</p>
 									</div>
 									<div class="icon">
 										<i class="ion ion-stats-bars"></i>
 									</div>
-									<a href="#" class="small-box-footer text-dark">More info <i class="fas fa-arrow-circle-right"></i></a>
+									<a href="{{route('users.index')}}" class="small-box-footer text-dark">More info <i class="fas fa-arrow-circle-right"></i></a>
 								</div>
 							</div>
 							
 							<div class="col-lg-4 col-6">							
 								<div class="small-box card">
 									<div class="inner">
-										<h3>$1000</h3>
+										<h3>&#8358;{{number_format($totalRevenue, 2)}}</h3>
 										<p>Total Sale</p>
+									</div>
+									<div class="icon">
+										<i class="ion ion-person-add"></i>
+									</div>
+									<a href="javascript:void(0);" class="small-box-footer">&nbsp;</a>
+								</div>
+							</div>
+							<div class="col-lg-4 col-6">							
+								<div class="small-box card">
+									<div class="inner">
+										<h3>&#8358;{{number_format($thisMonthRevenue, 2)}}</h3>
+										<p>This month revenue</p>
+									</div>
+									<div class="icon">
+										<i class="ion ion-person-add"></i>
+									</div>
+									<a href="javascript:void(0);" class="small-box-footer">&nbsp;</a>
+								</div>
+							</div>
+							<div class="col-lg-4 col-6">							
+								<div class="small-box card">
+									<div class="inner">
+										<h3>&#8358;{{number_format($lastMonthRevenue, 2)}}</h3>
+										<p>Last month revenue ({{$lastMonthName}})</p>
+									</div>
+									<div class="icon">
+										<i class="ion ion-person-add"></i>
+									</div>
+									<a href="javascript:void(0);" class="small-box-footer">&nbsp;</a>
+								</div>
+							</div>
+							<div class="col-lg-4 col-6">							
+								<div class="small-box card">
+									<div class="inner">
+										<h3>&#8358;{{number_format($lastThirtyDaysRevenue, 2)}}</h3>
+										<p>Last 30 days revenue</p>
 									</div>
 									<div class="icon">
 										<i class="ion ion-person-add"></i>
